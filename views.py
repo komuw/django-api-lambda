40367dc61b5a5a1f51db4216f013b13faf36e239
komu <komuw05@gmail.com>
2018-01-12 20:19:11 +0300

    add airflow stuff

--- views.py
+++ views.py
@@ -7,9 +7,10 @@ from ussd.core import UssdView, UssdRequest
 
 
 def home_view(request):
+    import sys
     logger = structlog.get_logger(__name__).bind(sessionId=str(uuid.uuid4()))
 
-    logger.info("new_request", name="komu", age=45)
+    logger.info("new_request", version=sys.version)
     return HttpResponse("<h1>Hello from django and up modules!</h1>")
 
 
