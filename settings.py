2c993b742244e1efa7df3688a0be50286c5c968e
komu <komuw05@gmail.com>
2018-02-17 11:27:59 +0300

    log

--- settings.py
+++ settings.py
@@ -93,6 +93,7 @@ logger.info("setup_boto")
 
 try:
     if UP_ENVIRONMENT:
+        logger.info("boto_setup", method="access-keys")
         # this variables will be setup by CI,
         # during CI, we'll edit up.json and add this env vars
         DYNAMODB_SESSIONS_AWS_ACCESS_KEY_ID = os.getenv(
@@ -105,10 +106,9 @@ try:
             aws_access_key_id=DYNAMODB_SESSIONS_AWS_ACCESS_KEY_ID,
             aws_secret_access_key=DYNAMODB_SESSIONS_AWS_SECRET_ACCESS_KEY,
             region_name=DYNAMODB_SESSIONS_AWS_REGION_NAME)
-        logger.info("boto_setup", method="access-keys")
     else:
+        logger.info("boto_setup", method="profile")
         DYNAMODB_SESSIONS_BOTO_SESSION = boto3.Session(
             profile_name='apex-up-profile')
-        logger.info("boto_setup", method="profile")
 except Exception as e:
     logger.exception("boto_setup_error", error=str(e))
