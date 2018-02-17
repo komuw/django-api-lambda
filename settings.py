a4ca55403501b098997a51a3c3b140cb6b1006f5
komu <komuw05@gmail.com>
2018-02-17 10:34:39 +0300

    deploy all

--- settings.py
+++ settings.py
@@ -74,8 +74,8 @@ if UP_ENVIRONMENT:
         "DYNAMODB_SESSIONS_AWS_SECRET_ACCESS_KEY")
     DYNAMODB_SESSIONS_AWS_REGION_NAME = os.getenv(
         "DYNAMODB_SESSIONS_AWS_REGION_NAME", "eu-west-1")
-else:
-    DYNAMODB_SESSIONS_BOTO_SESSION = boto3.Session(
+
+DYNAMODB_SESSIONS_BOTO_SESSION = boto3.Session(
         profile_name='apex-up-profile')
 
 
