8013f72c0d07c946ca327cbe7f6490debf273512
komu <komuw05@gmail.com>
2018-03-13 19:17:47 +0300

    cool

--- worker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-import views
-
-
-urlpatterns = (
-    # url(r'^$', views.api_root, name='api-root'),
-     url(r'^$', views.WorkerCrawler.as_view(), name='WorkerCrawler'),
-)
